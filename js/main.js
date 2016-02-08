9a14f1e80a008795e6870f02d7afd2d33cce2290
alberto__segura <alberto.segura.delgado@gmail.com>
2016-02-08 18:58:53 +0100

    added multiple fixtures on rotation, add license, add readme, add TVs, add date (with bugs..)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
     // TODO:: Do your initialization job
 
@@ -9,6 +10,26 @@ window.onload = function() {
             } catch (ignore) {}
         }
     });
+    
+    rotaryDetentCallback = function rotaryDetentHandler(e) {
+	    var direction = e.detail.direction;
+	    if (direction === "CW") {
+	        // TODO: do something when rotated clockwise
+	    	
+	    	nextFixture();
+	    	showFixture(actFixtures, ACT_FIXTURE);
+	    	console.log("CW");
+
+	    } else if (direction === "CCW") {
+	        // TODO: do something when rotated counter clockwise
+
+	    	prevFixture();
+	    	showFixture(actFixtures, ACT_FIXTURE);
+	    	console.log("CCW");
+	    	
+	    }
+	};
+	window.addEventListener("rotarydetent", rotaryDetentCallback);
 
     // Sample code
     var mainPage = document.querySelector('#main');
@@ -30,6 +51,95 @@ var API_BASE = "http://goles.eu-gb.mybluemix.net/golesapi/";
 var getLigueEndpoint = "nextFixtures";
 
 var MATCH_STATES = { FINISHED:3, IN_GAME:1, NOT_STARTED:0 };
+
+var actFixtures = {}; // Save a cache of obtained fixtures!!
+var ACT_FIXTURE = 0;
+
+
+function showFixture(data, i)
+{
+	ACT_FIXTURE = i;
+	var nextFixture = data.nextFixturesList[i];
+	
+	var resultTxt = document.querySelector("#result");
+    resultTxt.innerHTML = "<br><br><br>";
+	
+	var name = nextFixture.tournamentName[0].description +"<br>"+ nextFixture.fixtureName[0].description;
+	var nextMatches = nextFixture.fixtureMatchList;
+	
+	resultTxt.innerHTML += "<br> <span class='tournament-name-text'>"+name+"</span>";
+	for(i in nextMatches)
+	{
+		var visitor = nextMatches[i].teamVisitor;
+		var local = nextMatches[i].teamLocal;
+		var score = nextMatches[i].score;
+		var status = nextMatches[i].matchState;
+		var tv = nextMatches[i].listTV;
+		var date = nextMatches[i].dateMatch;
+		date = moment(date).lang(navigator.language).calendar();
+		//console.log(local+score+visitor);
+		
+		var style = "";
+		
+		switch (status) {
+		case MATCH_STATES.FINISHED:
+			style = "finished";
+			date = "";
+			tv = "";
+			break;
+			
+		case MATCH_STATES.IN_GAME:
+			style = "in-game";
+			date = "";
+			break;
+			
+		case MATCH_STATES.NOT_STARTED:
+			style = "not-started";
+			score = "-";
+			break;
+
+		default:
+			break;
+		}
+		var minutes = nextMatches[i].minutes || date;
+		
+		resultTxt.innerHTML += "<br><br><span class='fixture-text "+style+"'>" + local + " "+score+" " + visitor + " </span> <br> <span class='minutes-text'>"+ minutes +"</span> <span class='tvs-text'>"+ tv +"</span>";
+	}
+	
+	resultTxt.innerHTML += "<br><br><br><br><br><br>";
+}
+
+function readFixtures(fullPath)
+{
+	// get file
+	tizen.filesystem.resolve(
+	   fullPath,
+	   function(f) {
+		   f.readAsText(function(str) {
+		            console.log("The file content " + str);
+		             
+		            var data = JSON.parse(str);
+		             
+		            actFixtures = data;
+		            showFixture(data, 0);
+					
+					// Finished, clean
+					f.deleteFile(fullPath, function() {
+			             console.log("File Deleted");
+			           }, function(e) {
+			             console.log("Error" + e.message);
+			           });
+					
+		             
+		           }, function(e) {
+		             console.log("Error " + e.message);
+		           }, "UTF-8"
+		       );
+	   },
+	   function(e) { console.log("Error" + e.message);},
+	   "rw"
+   );
+}
 
 function getLigue(){
 	//console.log("Hi!");
@@ -68,74 +178,39 @@ function getLigue(){
 	 var downloadId = tizen.download.start(downloadRequest, listener);
 }
 
-
-function readFixtures(fullPath)
+function nextFixture()
 {
-	// get file
-	tizen.filesystem.resolve(
-	   fullPath,
-	   function(f) {
-		   f.readAsText(function(str) {
-		            console.log("The file content " + str);
-		            
-		            var resultTxt = document.querySelector("#result");
-		            resultTxt.innerHTML = "<br><br><br>";
-		             
-		            var data = JSON.parse(str);
-		             
-		            var nextFixture = data.nextFixturesList[0];
-					var name = nextFixture.tournamentName[0].description +"<br>"+ nextFixture.fixtureName[0].description;
-					var nextMatches = nextFixture.fixtureMatchList;
-					
-					resultTxt.innerHTML += "<span class='tournament-name-text'>"+name+"</span>";
-					for(i in nextMatches)
-					{
-						var visitor = nextMatches[i].teamVisitor;
-						var local = nextMatches[i].teamLocal;
-						var score = nextMatches[i].score;
-						var status = nextMatches[i].matchState;
-						var minutes = nextMatches[i].minutes || "";
-						//console.log(local+score+visitor);
-						
-						var style = "";
-						
-						switch (status) {
-						case MATCH_STATES.FINISHED:
-							style = "finished";
-							break;
-							
-						case MATCH_STATES.IN_GAME:
-							style = "in-game";
-							break;
-							
-						case MATCH_STATES.NOT_STARTED:
-							style = "not-started";
-							break;
-
-						default:
-							break;
-						}
-						
-						resultTxt.innerHTML += "<br><br><span class='fixture-text "+style+"'>" + local + " "+score+" " + visitor + " </span> <span class='minutes-text'>"+minutes+"</span>";
-					}
-					
-					resultTxt.innerHTML += "<br><br><br><br><br><br>";
-					
-					// Finished, clean
-					f.deleteFile(fullPath, function() {
-			             console.log("File Deleted");
-			           }, function(e) {
-			             console.log("Error" + e.message);
-			           });
-					
-		             
-		           }, function(e) {
-		             console.log("Error " + e.message);
-		           }, "UTF-8"
-		       );
-	   },
-	   function(e) { console.log("Error" + e.message);},
-	   "rw"
-   );
+	ACT_FIXTURE = (ACT_FIXTURE + 1) % actFixtures.nextFixturesList.length;
 }
+
+function prevFixture()
+{
+	ACT_FIXTURE = (ACT_FIXTURE - 1) % actFixtures.nextFixturesList.length;
+}
+
+function bezerInit()
+{
+	// BEZEL SCROLL
+	var page = document.getElementById("main"),
+	element = document.getElementById("sectionchanger"),
+	sectionChanger, idx=1;
+	
+	page.addEventListener("pageshow", function() {
+	   /* Create the SectionChanger object */
+	   sectionChanger = tau.widget.SectionChanger(element, {
+	      circular: true,
+	      orientation: "horizontal",
+	      useBouncingEffect: true
+	   });
+	});
+	
+	page.addEventListener("pagehide", function() {
+	   /* Release the object */
+	   sectionChanger.destroy();
+	});
+}
+
+
+
+
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Goles for Tizen (Gear S2)
+
+![](https://img.shields.io/badge/license-GPLv2-blue.svg)
+
+This app uses [Goles Messenger API](https://es-es.facebook.com/GolesMessenger) to obtain and show football results on Samsung Gear S2 smartwatch!
+
+Video example: https://twitter.com/alberto__segura/status/696754334479032323
+
+# License
+
+GNU GENERAL PUBLIC LICENSE Version 2
